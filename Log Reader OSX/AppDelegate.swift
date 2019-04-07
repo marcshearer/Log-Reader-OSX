73cdf4fac7ffd0fb0758f8afa9a343cc9b9f2b26
Marc Shearer <marc.shearer@tesco.net>
2019-04-07 04:50:28 +0100

    Initial Commit

---
+++ Log Reader OSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Log Reader OSX
+//
+//  Created by Marc Shearer on 07/04/2019.
+//  Copyright © 2019 Marc Shearer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Log Reader OSXTests/Log_Reader_OSXTests.swift
@@ -0,0 +1,34 @@
+//
+//  Log_Reader_OSXTests.swift
+//  Log Reader OSXTests
+//
+//  Created by Marc Shearer on 07/04/2019.
+//  Copyright © 2019 Marc Shearer. All rights reserved.
+//
+
+import XCTest
+@testable import Log_Reader_OSX
+
+class Log_Reader_OSXTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
